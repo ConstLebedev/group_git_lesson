d33bfbd0f4716a24f1dc0d87801b6b6dcc58b717
Blaze <blaz.yt@yandex.ru>
2024-12-16 18:52:10 +0500

    gfgd

--- root/js/main.js
+++ root/js/main.js
@@ -1,2 +1,6 @@
 const tabs = document.querySelector('.tabs')
 var instance = M.Tabs.init(tabs);
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+});
