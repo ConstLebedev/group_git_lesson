5e51cdf5287c2be752c082b0f99a90cd08f4dd5e
lebedeffko <lebedeffko@yandex.ru>
2024-12-16 18:19:56 +0500

    modal and float btn

--- lebedev/js/main.js
+++ lebedev/js/main.js
@@ -1,3 +1,7 @@
 const tabs = document.querySelector('.tabs')
 var instance = M.Tabs.init(tabs);
 
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+});
