2c31111bc7d360c779fe719d692c8646f39288f3
Djarekingh <smekalinkirill7@gmail.com>
2024-12-16 18:52:23 +0500

    HopM

--- смекалин/js/main.js
+++ смекалин/js/main.js
@@ -1,3 +1,13 @@
 
 const tabs = document.querySelector('.tabs')
 var instance = M.Tabs.init(tabs);
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems);
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, );
+  });
