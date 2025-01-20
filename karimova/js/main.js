4af6290cf04eb1c533ea21e2847b506e267aec5c
karimova <karimovakk@yandex.ru>
2025-01-20 17:16:46 +0500

    todo1

--- karimova/js/main.js
+++ karimova/js/main.js
@@ -1,3 +1,40 @@
 const tabs = document.querySelector('.tabs')
 var instance = M.Tabs.init(tabs);
 
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
+
+
+  const tasks =[];
+  const from = document.querySelector('#form')
+  const bnt = document.querySelector('#submit')
+ const uncomleted = document.querySelector('')
+
+  bnt.addEventListener('click',add_task)
+
+  function add_task(){
+     const task ={
+      text: from.
+      status:  false
+     }
+     function udpate_list () {
+      uncomleted.innerHTML = ""
+      completed.innerHTML = ""
+      for (t of task) {
+        cost item = document.createAttribute
+      }
+     }
+     </div> class = "row valign-wrapper">
+
+
+     <div class ="col s1" >
+      
+     </div>
+      if  (t.status) {
+          completed.append(item)
+      }
+        
+     }   
+
